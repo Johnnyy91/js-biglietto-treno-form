0a5afd02bb8750f78b50e5092ccf0e47893ffef7
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-24 19:14:57 +0200

    modification part 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 /*
+FIRST EXERCISE
+
 console.log('JS OK')
 
 const età = prompt('Età passeggero?');
@@ -24,13 +26,45 @@ else if (etàNumber> 65)
 
 console.log(costo);
 document.getElementById('js').innerHTML= costo;
-*/
+
+CLOSE FIRST EXERCISE */
+
+
+//FUNCTION
 
 const età = document.getElementById('età');
 const km = document.getElementById('km');
 const costo = document.getElementById('costo');
+const passenger =document.getElementById('passenger')
+const pinco =document.getElementById('name')
+const ticket = document.getElementById('ticket');
 
-costo.addEventListener('click')
+
+ticket.addEventListener("click", function(){
+    const price = km.value * 0.21
+    costo.innerHTML = price
+    console.log(costo.innerHTML)
+
+    if (età.value < 18) 
+    {
+        costo.innerHTML = price - (price * 0.20)
+    }
+    else if (età.value > 65)
+    {
+        costo.innerHTML = price - ( price * 0.40)
+    }
+
+    passenger.innerHTML = pinco.value
+
+});
+
+//CLOSE FUNCTION
+
+
+
+
+
+
 
 
 
